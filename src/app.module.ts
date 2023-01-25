9b22266fda7d315a5201caca9e0e807b9d320ee9
Eunji Kim <39396725+oliveunji@users.noreply.github.com>
2023-01-25 17:39:25 +0900

    use postgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm/dist';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { BoardsModule } from './boards/boards.module';
-// import { typeORMConfig } from './configs/typeorm.config';
+import { typeORMConfig } from './configs/typeorm.config';
+// import { config } from 'dotenv';
+
+// config();
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT, 10) || 5432,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: 'board-app',
-      entities: [__dirname + '/../**/*.entitiy.{js,ts}'],
-      synchronize: true,
-      ssl: true,
-    }),
-    BoardsModule,
-  ],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule],
   controllers: [],
   providers: [],
 })
